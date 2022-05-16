b25166a9912d514b9f2a4186ae7531441f0b79b7
pizzacode-byte <piazzacode@gmail.com>
2022-05-16 17:15:54 +0200

    infinite song loop

--- src/playmusic.js
+++ src/playmusic.js
@@ -1,7 +1,7 @@
 const songs = ['music/song-1.mp3', 'music/song-2.mp3', 'music/song-3.mp3', 'music/song-4.mp3', 'music/song-5.mp3'];
 const play = document.getElementById('music');
 const audioController = document.getElementById('song');
-let counter = 0;
+let stop = 0;
 
 class Song{
     constructor(title,controller){
@@ -24,16 +24,17 @@ class Song{
 
 
 play.addEventListener('click', () => {
-    counter++;
-    //take a random song title from the list
+    stop++;
+    //take a random song  from the list
     let audio = new Song(songs[Math.floor(Math.random() * songs.length)], audioController);
+    let playMusic = audio.play();
+    console.log('started');
 
-    let play = audio.play();
 
-    // if the button is clicked 2 times you want to stop the audio
-    if(play !== undefined){
-        play.then(() => {
-            if(counter % 2 == 0){
+    // if the button is clicked 2 times the audio is stopped
+    if(playMusic !== undefined){
+        playMusic.then(() => {
+            if(stop % 2 == 0){
                 audio.stop();
             }
         })
@@ -41,7 +42,8 @@ play.addEventListener('click', () => {
 
 });
 
-play.addEventListener('onended', () => {
-    let audio = new Song(songs[Math.floor(Math.random() * songs.length)], audioController);
-    let play = audio.play();
-});
+
+audioController.onended = () => {
+  audio = new Song(songs[Math.floor(Math.random() * songs.length)], audioController);
+  let playMusic = audio.play();
+}
